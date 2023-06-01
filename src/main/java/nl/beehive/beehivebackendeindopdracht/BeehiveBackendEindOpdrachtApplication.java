162eaf45c83c98159638545f43272e5ad4831f30
EAM26 <emuller2607@gmail.com>
2023-06-01 12:13:03 +0200

    Initial project setup

---
+++ src/main/java/nl/beehive/beehivebackendeindopdracht/BeehiveBackendEindOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package nl.beehive.beehivebackendeindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeehiveBackendEindOpdrachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeehiveBackendEindOpdrachtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/beehive/beehivebackendeindopdracht/BeehiveBackendEindOpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.beehive.beehivebackendeindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeehiveBackendEindOpdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
